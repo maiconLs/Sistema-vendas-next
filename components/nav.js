caecfef9c4cf4c0e966c2e82095f6d48ea3fe212
Maicon <maiconlsilva10@gmail.com>
2022-12-03 12:40:31 -0300

    adding activeLink

--- components/nav.js
+++ components/nav.js
@@ -1,8 +1,9 @@
 import Link from "next/link";
 import React from "react";
-import { signOut } from "next-auth/react";
+import {  signOut } from "next-auth/react";
 
 export default function Nav() {
+  const pathName = window.location.pathname
   return (
     <div className='fixed top-0 overflow-auto z-30 w-48 h-screen bg-slate-500 max-[600px]:w-full max-[600px]:h-36 '>
       <ul className='w-full max-[600px]:flex max-[600px]:justify-between max-[600px]:flex-col'>
@@ -10,6 +11,7 @@ export default function Nav() {
           <Link href='/'>
             {" "}
             <li className='p-6 text-slate-800 font-bold text-2xl max-[600px]:text-xl'>
+              
               Minha Loja
             </li>
           </Link>
@@ -18,26 +20,19 @@ export default function Nav() {
         <div className='max-[600px]:flex'>
           <Link href='/'>
             {" "}
-            <li className='p-6 text-white font-bold text-xl'>Inicio</li>
+            <li className={pathName == '/' ? `active` : 'link'}>Inicio</li>
           </Link>
           <Link href='/produtos'>
             {" "}
-            <li className='p-6 text-white font-bold text-xl'>Produtos</li>
+            <li className={pathName == '/produtos' ? `active` : 'link'}>Produtos</li>
           </Link>
           <Link href='/vendas'>
             {" "}
-            <li className='p-6 text-white font-bold text-xl'>Vendas</li>
+            <li className={pathName == '/vendas' ? `active` : 'link'}>Vendas</li>
           </Link>
-          <li
-            className='p-6 text-white font-bold text-xl cursor-pointer'
-            onClick={() =>
-              signOut({
-                callbackUrl: `${window.location.origin}/login`,
-              })
-            }
-          >
-            Sair
-          </li>
+          <li className='link' onClick={() => signOut({
+      callbackUrl: `${window.location.origin}/login`
+    })}>Sair</li>
         </div>
       </ul>
     </div>
